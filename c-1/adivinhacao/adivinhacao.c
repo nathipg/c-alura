f229ba5e087c2124ae2022d046e5ea3ebeb6dee1
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-12-29 18:08:44 -0300

    Testando número

--- c-1/adivinhacao/adivinhacao.c
+++ c-1/adivinhacao/adivinhacao.c
@@ -13,4 +13,21 @@ int main() {
 	printf("Qual é o seu chute? ");
 	scanf("%d", &chute);
 	printf("Seu chute foi: %d\n", chute);
+
+	int acertou = (chute == numero_secreto);
+
+	if(acertou) {
+		printf("Parabéns! Você acertou!\n");
+	} else {
+		printf("Você errou.\n");
+
+		int maior = chute > numero_secreto;
+		if(maior) {
+			printf("Seu chute foi maior que o número secreto\n");
+		} else {
+			printf("Seu chute foi menor que o número secreto\n");
+
+		}
+	}
+
 }
